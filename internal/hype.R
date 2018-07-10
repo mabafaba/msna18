c3be6ee4da94917356667ba27c4aad1546467e7d
ellieallien <35920142+ellieallien@users.noreply.github.com>
2018-07-10 09:27:26 +0200

    working directory no longer changes

--- internal/hype.R
+++ internal/hype.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-setwd("../")
+# setwd("../")
 # getwd()
 
 # clear/create folders
@@ -44,10 +44,12 @@ questionnaire<-load_questionnaire(data,questions.file = "./internal/input_files/
 #composite_indicators
 composite_indicators_definitions_weighted_counts<-load_composite_indicator_definition_weighted_count()
 data<-add_variable_indicators_weighted_count(data,composite_indicators_definitions_weighted_counts)
-
 debug(recode_select_multiple)
-
+data$biggest.needs %>% grep("nfi",.) %>% length
 data %>% map_to_file("./output/modified_data/data_with_composite_indicators.csv")
+
+recode_else(data,data$biggest.needs,to = 1) %>% sum(na.rm = T)
+
 
 # load analysis definitions
 # aggregating all variables (direct reporting)
