6111151b449a82eec696ed74046be2b504c5a4eb
Martin B <barner.mar@gmail.com>
2018-07-25 10:06:54 +0200

    allow underscores in colheaders

--- internal/R/hypegrammaR/utilities.R
+++ internal/R/hypegrammaR/utilities.R
@@ -1,5 +1,4 @@
-to_alphanumeric_lowercase <- function(x){tolower(gsub("[^a-zA-Z0-9]", "\\.", x))}
-
+to_alphanumeric_lowercase <- function(x){tolower(gsub("[^a-zA-Z0-9_]", "\\.", x))}
 to_alphanumeric_lowercase_colnames_df <- function(df){
   names(df) <- to_alphanumeric_lowercase(names(df))
   return(df)
