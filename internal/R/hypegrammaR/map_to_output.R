bd1441aac236881b3e7f298cfa8a99c69a86a092
Martin B <barner.mar@gmail.com>
2018-08-09 12:15:38 +0200

    maptoputput

---
+++ internal/R/hypegrammaR/map_to_output.R
@@ -0,0 +1,35 @@
+
+
+map_resultlist_to_summarystatistic_df<-function(results){
+  all_summary_statistics <- results %>% lapply(function(x){x$summary.statistic}) %>% do.call(rbind,.)
+}
+
+
+map_resultlist_to_datamerge<-function(results,rows=c("repeat.var","repeat.var.value"),values="numbers",ignore=c("se","min")){
+  all_summary_statistics <-  results %>% lapply(function(x){x$summary.statistic}) %>% do.call(rbind,.)
+  columns<-  names(all_summary_statistics)[!(names(all_summary_statistics) %in% c(rows,ignore,values))]
+
+  all_summary_statistics$master_table_column_name<-all_summary_statistics[,columns] %>% as.list %>% c(sep=":::") %>% do.call(paste,.) 
+  all_summary_statistics$master_table_column_name %>% table
+  # what to keep rows for:
+  wide_format<-all_summary_statistics[,c(rows,"master_table_column_name",values)] %>%
+    spread(key = master_table_column_name,value = numbers) %>% map_to_file("./output/master_table_wide.csv")
+return(wide_format)
+  
+  
+    
+}
+
+
+map_resultlist_to_datamerge(results) %>% map_to_file("./output/master_table_wide.csv")
+
+
+
+
+
+all_summary_statistics$master_table_column_name %>% table
+
+all_summary_statistics[all_summary_statistics$master_table_column_name=="fcs_ranking:::population_group:::acceptable:::IDP",]
+map_to_output_from<-function(result,output.path="./output/",fs_style_barchart)
+  ?spread
+all_summary_statistics$repeat.var.value %>% unique %>% length
